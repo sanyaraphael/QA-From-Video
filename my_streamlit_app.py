8ed7cd04d603136c01bfdf1ff92c62bca85357c3
Sanya Raphael <sanyaraphael@gmail.com>
2023-08-06 16:42:32 +0530

    python file added

---
+++ my_streamlit_app.py
@@ -0,0 +1,139 @@
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import extract,metadata,YouTube
+from os import environ
+from langchain.vectorstores import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import pinecone
+from langchain.schema import Document
+import streamlit as st
+
+
+
+# function which will fetch the youtube transcript
+def download_youtube_transcript(video_url):
+    try:
+        # Get the transcript for the YouTube video
+        video_id=extract.video_id(video_url)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        
+        # Combine the transcript into a single string
+        transcript_text = ""
+        for entry in transcript:
+            transcript_text += entry['text'] + " " +str(entry['start'] )
+        
+        # Write the transcript to a text file
+        with open("transcript.txt", "w", encoding="utf-8") as file:
+            file.write(transcript_text)
+        
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    return transcript_text
+
+
+def main():
+    #load environment variables from .env
+    load_dotenv()
+    openai_api_key=environ.get("OPEN_AI_API_KEY")
+    pinecone_api_key=environ.get("PINECONE_API_KEY")
+    pinecone_env=environ.get("PINECONE_ENVIRONMENT")
+    index_name=environ.get("INDEX_NAME")
+    project_name=environ.get("PROJECT_NAME")
+
+    
+
+    embedding=OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+    #initialize pinecone
+    pinecone.init(      
+	api_key=pinecone_api_key,      
+	environment=pinecone_env,
+    project_name=project_name     
+    )      
+    index = pinecone.Index(index_name)
+   
+    print(index_name)
+    # this connects to the Pinecone service, creates or retrieves an index 
+    # with the given name, vectorizes the input documents, and then inserts them 
+    # into the index using the upsert() method.
+
+  
+
+    #Dividing the transcript into chunks
+    link=st.text_input(label="Youtube Video Url",placeholder="Paste your youtube video link here")
+   
+    if link and link !='':
+        st.session_state.link='';
+        thumbnail_url = YouTube(link).thumbnail_url
+        video_id = extract.video_id(link)
+    #getting the transcript from youtube
+    video_transcript=download_youtube_transcript(video_id)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+    # text will consist of chucks to which the text_splitter divided 
+    texts=text_splitter.split_documents(video_transcript)
+
+    document=[]
+    chunks=""
+  
+    start=0
+
+    for i in video_transcript:
+        if len(texts) >= 2000:
+            document.append(Document(page_content=chunks,metedata={"start":start,"youtube_link":link, "thumbnail_url": thumbnail_url}))
+            texts=" "       
+        else:
+            if texts ==' ':
+                start= int(i['start'])
+            texts =texts+i['text']
+        #storing docs to pinecone
+        docsearch = Pinecone.from_documents(documents=document,embedding=embedding, index_name=index)
+        
+        #resetting link to None to avoid the duplicate insert of docs 
+        link=None
+        user_question = st.text_input("Ask a question about data:")
+        if user_question:
+            #do the search to DB
+            docs = docsearch.similarity_search(user_question)
+            llm = OpenAI()
+
+            #load question answer from langchain library
+            chain = load_qa_chain(llm, chain_type="stuff")
+        # get_openai_callback() is used to check the billing info of openai
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=document, question=user_question)
+            print(cb)
+
+        st.write(response)
+        references = []
+        for j in docs:
+            content = j.page_content
+            content = (content[: 275] + '-----------') if len(content)>75 else content
+            ref_object = {
+            'content': content,
+            'youtube_link':str(j.metadata['youtube_link']),
+            'start':str(int(float(str(j.metadata['Start'])))),
+            'thumbnail_url':j.metadata['thumbnail_url']
+            }
+        references.append(ref_object)
+        st.header ("References") 
+        for j in references:
+           st.markdown('<a href="'+str(j['youtube_link' ])+'&t='+str(j['start'])+'">'+str(j['start'])+'<img style="width: <br>' +j['content'],unsafe_allow_html=True)
+
+
+# this method called after submit of youtube link
+def submit ():
+    st.session_state.link = st.session_state.widget
+    st.session_state.widget = ""
+
+if __name__ == '__main__':
+    # main()
+    download_youtube_transcript("https://www.youtube.com/watch?v=CaQA2paqZTE")
+
+
+
